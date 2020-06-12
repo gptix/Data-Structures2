3afe157bacee4b46eb0ea2961afc400cb2460f04
Jud Taylor <jud.taylor@protonmail.com>
2020-06-11 21:23:41 -0400

    Converted searches to list-based. All passed.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,205 @@
+class Node:
+    def __init__(self, value=None, next_node=None):
+        # the value at this linked list node
+        self.value = value
+        # reference to the next node in the list
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        # set this node's next_node reference to the passed in node
+        self.next_node = new_next
+        
+
+
+
+
+
+class LinkedList:
+    def __init__(self):
+        # reference to the head of the list
+        self.head = None
+        # reference to the tail of the list
+        self.tail = None
+
+    def add_to_tail(self, value):
+        # wrap the input value in a node
+        new_node = Node(value, None)
+        # check if there is no head (i.e., the list is empty)
+        if not self.head:
+            # if the list is initially empty, set both head and tail to the new node
+            self.head = new_node
+            self.tail = new_node
+        # we have a non-empty list, add the new node to the tail
+        else:
+            # set the current tail's next reference to our new node
+            self.tail.set_next(new_node)
+            # set the list's tail reference to the new node
+            self.tail = new_node
+
+    def remove_head(self):
+        # return None if there is no head (i.e. the list is empty)
+        if not self.head:
+            return None
+        # if head has no next, then we have a single element in our list
+        if not self.head.get_next():
+            # get a reference to the head
+            head = self.head
+            # delete the list's head reference
+            self.head = None
+            # also make sure the tail reference doesn't refer to anything
+            self.tail = None
+            # return the value
+            return head.get_value()
+        # otherwise we have more than one element in our list
+        value = self.head.get_value()
+        # set the head reference to the current head's next node in the list
+        self.head = self.head.get_next()
+        return value
+
+    def remove_tail(self):
+        if not self.head:
+            return None
+        
+        if self.head is self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+        
+        current = self.head
+
+        while current.get_next() is not self.tail:
+            current = current.get_next()
+
+        value = self.tail.get_value()
+        self.tail = current
+        return value
+
+    def contains(self, value):
+        if not self.head:
+            return False
+    
+        # get a reference to the node we're currently at; update this as we traverse the list
+        current = self.head
+        # check to see if we're at a valid node 
+        while current:
+            # return True if the current value we're looking at matches our target value
+            if current.get_value() == value:
+                return True
+            # update our current node to the current node's next node
+            current = current.get_next()
+        # if we've gotten here, then the target node isn't in our list
+        return False
+
+    def get_max(self):
+        if not self.head:
+            return None
+        # reference to the largest value we've seen so far
+        max_value = self.head.get_value()
+        # reference to our current node as we traverse the list
+        current = self.head.get_next()
+        # check to see if we're still at a valid list node
+        while current:
+            # check to see if the current value is greater than the max_value
+            if current.get_value() > max_value:
+                # if so, update our max_value variable
+                max_value = current.get_value()
+
+
+
+
+
+
+
+
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+    
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.size == 0:
+            self.storage.head = new_node
+            self.storage.tail = new_node
+            self.size += 1
+        else: # LL is not empty. We will put things on the tail, and take them from the head
+            self.storage.tail.next_node = new_node
+            self.storage.tail = new_node
+            self.size += 1
+        # print(self.size)
+        return True
+
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        else:
+            retval = self.storage.head.value
+            self.storage.head = self.storage.head.next_node
+            self.size -= 1
+            return retval
+
+
+
+
+
+
+
+
+class Stack:
+    def __init__(self):
+        self.storage = LinkedList()
+
+    def __len__(self):
+        retval = 0
+        curr = self.storage.head
+        if curr is not None:
+            retval += 1
+            while curr.next_node is not None:
+                retval += 1
+                curr = curr.next_node
+        return retval
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.storage.head == None:
+            self.storage.head = new_node
+            self.storage.tail = new_node
+        
+        else:
+            new_node.next_node = self.storage.head
+            self.storage.head = new_node
+
+    def pop(self):
+        if self.storage.head == None:
+            return None
+        else:
+            retval = self.storage.head.value
+            self.storage.head = self.storage.head.next_node
+            return retval
+
+
+
+
+
+
+
+
+
+
+
+
 """
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
@@ -104,32 +306,63 @@ class BSTNode:
     # in an iterative breadth first traversal
     def bft_print(self, node):
         
-        queue = [node]
+    #     queue = [node]
 
-        while queue != []:
-            current = queue.pop(0)
-            if current.right:
-                queue.append(current.right)
-            if current.left:
-                queue.append(current.left)
-            print(current.value)
+    #     while queue != []:
+    #         current = queue.pop(0)
+    #         if current.right:
+    #             queue.append(current.right)
+    #         if current.left:
+    #             queue.append(current.left)
+    #         print(current.value)
+
+        # def bft_print(self, node):
+        
+        q = Queue()
+        q.enqueue(node)
+        
+        while q.size != 0:
+            current = q.storage.head.value
+            if current.right is not None:
+                q.enqueue(current.right)
+            if current.left is not None:
+                q.enqueue(current.left)
+            print(q.dequeue().value)
             
             
-
-
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
         
         stack = [node]
         
-        while stack != []:
-            current = stack.pop(-1)
-            if current.left:
-                stack.append(current.left)
-            if current.right:
-                stack.append(current.right)
+        # while stack != []:
+        #     current = stack.pop(-1)
+        #     if current.left:
+        #         stack.append(current.left)
+        #     if current.right:
+        #         stack.append(current.right)
+        #     print(current.value)
+
+
+        s = Stack()
+        s.push(node)
+        
+        while len(s) > 0:
+            current = s.pop()
+            if current.left is not None:
+                s.push(current.left)
+            if current.right is not None:
+                s.push(current.right)
             print(current.value)
+
+
+
+
+
+
+
+
 
     # Stretch Goals -------------------------
     # Note: Research may be required
